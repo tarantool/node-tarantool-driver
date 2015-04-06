09893f2af679afad52c7998d1583294e2e44f224
Devilstar <kolmakoff.tema@gmail.com>
2015-04-06 11:45:47 +0300

    test connection

---
+++ test/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by klond on 05.04.15.
+ */
+var TarantoolConnection = require('../lib/connection');
+
+var conn = new TarantoolConnection({port: 33013, host: '95.85.55.64'});
+conn.connect()
+    .then(function(){
+        console.log('resolve');
+    }, function(e){
+        console.log('reject');
+    })
