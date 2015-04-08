23c8996b48f507a366d32759375a81c29015abc5
KlonD90 <newbiecraft@gmail.com>
2015-04-09 02:43:57 +0300

    we are on the way to select

--- test/app.js
+++ test/app.js
@@ -7,7 +7,20 @@ var conn = new TarantoolConnection({port: 33013, host: '95.85.55.64'});
 conn.connect()
     .then(function(){
         console.log('resolve');
-        conn.ping();
+        return conn.ping();
     }, function(e){
         console.log('reject');
+    })
+    .then(function(ss){
+        console.log(ss);
+        console.log('start request select')
+        return conn.select(512, 0, 1, 0, 'eq', [2]);
+    }, function(e){
+        console.log(e);
+    })
+    .then(function(ff){
+        console.log('select result');
+        console.log(ff);
+    }, function(e){
+        console.log('err on select', e)
     })
