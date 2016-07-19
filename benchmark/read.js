0ba9371f1f82ddeeb7a8c2c781f589c97e55ab30
KlonD90 <newbiecraft@gmail.com>
2016-07-19 18:50:24 +0300

    benchmarking lol

--- benchmark/read.js
+++ benchmark/read.js
@@ -2,8 +2,10 @@
 var Benchmark = require('benchmark');
 
 var tDriver = require('../lib/connection.js');
+var oldDriver = require('../../old_tnt/lib/connection.js');
+var oldConn = new oldDriver({port: 3301});
 var tConn = new tDriver({});
-tConn.connect()
+Promise.all([tConn.connect(), oldConn.connect()])
 .then(function(){
   var suite = new Benchmark.Suite;
   suite.add('paralell 5000', {defer: true, fn: function(defer){
@@ -13,7 +15,8 @@ tConn.connect()
       promises.push(
         tConn.select(512, 0, 1, 0, 'eq', ['test'])
           .then(function(){
-            console.log('l', l);
+//            console.log('l', l);
+//            console.log('c', counter++);
             return l;
           })
       );
@@ -30,12 +33,16 @@ tConn.connect()
       console.error(e, e.stack);
     }
   }});
+  suite.add('counter', {defer: true, fn: function(defer){
+    var chain = tConn.select('counter', 'primary', 1000000, 0, 'all',[]);
+    chain.then(function(){ defer.resolve();});
+  }});
   suite.add('sequence', {defer: true, fn: function(defer){
-    var chain = promise.resolve();
+    var chain = Promise.resolve();
     for (var i=0;i<5000;i++)
     {
       chain = chain.then(function(){
-        return tconn.select(512, 0, 1, 0, 'eq', ['test']);
+        return tConn.select(512, 0, 1, 0, 'eq', ['test']);
       });
     }
     chain.then(function(){ defer.resolve();});
@@ -73,6 +80,91 @@ tConn.connect()
           for (var l=0;l<50;l++)
           promises.push(
             tConn.select(512, 0, 1, 0, 'eq', ['test'])
+          );
+          return Promise.all(promises)
+        });
+    }
+
+    chain.then(function(){ defer.resolve() })
+    .catch(function(e){
+      console.error(e, e.stack);
+    });
+    } catch(e){
+      console.error(e, e.stack);
+    }
+  }});
+  suite.add('old paralell 5000', {defer: true, fn: function(defer){
+    try{
+    var promises = [];
+    for (let l=0;l<5000;l++)
+      promises.push(
+        oldConn.select(512, 0, 1, 0, 'eq', ['test'])
+          .then(function(){
+//            console.log('l', l);
+//            console.log('c', counter++);
+            return l;
+          })
+      );
+    var chain = Promise.all(promises)
+
+
+    chain.then(function(){ defer.resolve() })
+    .catch(function(e){
+      defer.reject(e);
+      console.error(e, e.stack);
+    });
+    } catch(e){
+      defer.reject(e);
+      console.error(e, e.stack);
+    }
+  }});
+  suite.add('old counter', {defer: true, fn: function(defer){
+    var chain = oldConn.select('counter', 'primary', 1000000, 0, 'all',[]);
+    chain.then(function(){ defer.resolve();});
+  }});
+  suite.add('old sequence', {defer: true, fn: function(defer){
+    var chain = Promise.resolve();
+    for (var i=0;i<5000;i++)
+    {
+      chain = chain.then(function(){
+        return oldConn.select(512, 0, 1, 0, 'eq', ['test']);
+      });
+    }
+    chain.then(function(){ defer.resolve();});
+  }});
+  suite.add('old paralel by 10', {defer: true, fn: function(defer){
+    var chain = Promise.resolve();
+    try{
+    for (var i=0;i<500;i++)
+    {
+        chain = chain.then(function(){
+          var promises = [];
+          for (var l=0;l<10;l++)
+          promises.push(
+            oldConn.select(512, 0, 1, 0, 'eq', ['test'])
+          );
+          return Promise.all(promises)
+        });
+    }
+
+    chain.then(function(){ defer.resolve() })
+    .catch(function(e){
+      console.error(e, e.stack);
+    });
+    } catch(e){
+      console.error(e, e.stack);
+    }
+  }});
+  suite.add('old paralel by 50', {defer: true, fn: function(defer){
+    var chain = Promise.resolve();
+    try{
+    for (var i=0;i<100;i++)
+    {
+        chain = chain.then(function(){
+          var promises = [];
+          for (var l=0;l<50;l++)
+          promises.push(
+            oldConn.select(512, 0, 1, 0, 'eq', ['test'])
           );
           return Promise.all(promises)
         });
