ea453cf0a3b5d939878f0d39a81651866b9f15af
leo <user.name>
2017-06-21 15:03:25 +0300

    ci changes

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl http://tarantool.org/dist/public.key | sudo apt-key add -
+echo "deb http://tarantool.org/dist/master/ubuntu/ `lsb_release -c -s` main" | sudo tee -a /etc/apt/sources.list.d/tarantool.list
+sudo apt-get update > /dev/null
+sudo apt-get -q -y install tarantool
