db0e88edccd17eb30787ac29722d2a22463cdb41
noevents <noevents@users.noreply.github.com>
2017-08-17 18:20:26 +0300

    multihost feature: client will try to connect to reserve hosts if provided

--- test.sh
+++ test.sh
@@ -5,3 +5,5 @@
 # sudo apt-get update > /dev/null
 sudo docker pull tarantool/tarantool:1.7
 sudo docker run --name tarantool -p33013:33013 -d -v `pwd`/test:/opt/tarantool tarantool/tarantool:1.7 tarantool /opt/tarantool/box.lua
+sudo docker run --name reserve -p33014:33014 -d -v `pwd`/test:/opt/tarantool tarantool/tarantool:1.7 tarantool /opt/tarantool/box1.lua
+sudo docker run --name reserve_2 -p33015:33015 -d -v `pwd`/test:/opt/tarantool tarantool/tarantool:1.7 tarantool /opt/tarantool/box2.lua
