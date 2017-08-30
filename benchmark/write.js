5cc5fb37575e4deb29e7b81086e1747e85e25fa7
noevents <anonananasik@gmail.com>
2017-08-30 17:43:49 +0300

    benchmark

---
+++ benchmark/write.js
@@ -0,0 +1,55 @@
+/* global Promise */
+'use strict';
+var Benchmark = require('benchmark');
+var suite = new Benchmark.Suite();
+var Driver = require('../lib/connection.js');
+var uri = process.argv[process.argv.length - 1].split(':');
+var opts = (uri.length == 1) ? {port: uri[0]} : {host: uri[0], port: uri[1]};
+var conn = new Driver(opts);
+var promises;
+var c = 0;
+
+conn.connect()
+	.then(function(){
+		suite.add('insert', {defer: true, fn: function(defer){
+			conn.insert('bench', [c++, {user: 'username', data: 'Some data.'}])
+				.then(function(){defer.resolve();})
+				.catch(function(e){
+					console.error(e, e.stack);
+					defer.reject(e);
+				});
+		}});
+		
+		suite.add('insert parallel 50', {defer: true, fn: function(defer){
+			try{
+				promises = [];
+				for (let l=0;l<50;l++){
+					promises.push(conn.insert('bench', [c++, {user: 'username', data: 'Some data.'}]));
+				}
+				var chain = Promise.all(promises);
+				chain.then(function(){ defer.resolve(); })
+					.catch(function(e){
+						console.error(e, e.stack);
+						defer.reject(e);
+					});
+				} catch(e){
+					defer.reject(e);
+					console.error(e, e.stack);
+				}
+		}});
+		suite
+			.on('cycle', function(event) {
+				console.log(String(event.target));
+			})
+			.on('complete', function() {
+				conn.eval('return clear()')
+					.then(function(){
+						console.log('complete');
+						process.exit();
+					})
+					.catch(function(e){
+						console.error(e);
+					});
+			})
+			.run({ 'async': true, 'queued': true });
+	});
